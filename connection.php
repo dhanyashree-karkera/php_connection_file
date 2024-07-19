794ee245e941ada15bcb1b34ddc150e0fed46734
Dhanyashree <75880251+dhanyashreekarkera@users.noreply.github.com>
2024-07-19 15:54:47 +0530

    connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $username="root";
 $password=" ";
 $db_name="my_database";
 
-$con=mysqli($host, $username, $password, $db_name);
+$conn=mysqli($host, $username, $password, $db_name);
 if(mysqli_connect_errno()){
     die("Failed to connect with MySQL: " .mysqli_connect_error());
 }
